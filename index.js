f9061954e1888cbaa43bb9332ef27c7f4d76b926
Junior Fogassi <jrfogassi@gmail.com>
2022-03-31 10:48:40 -0300

    adicionando comentários

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 function countTrue(arr) {
+
+	//Contador inicia-se no 0
+
 	let contador = 0;
+	
+	//Verifica se o índice é true, se for, o contador é aumentado
 	
 	for (var i = 0; i < arr.length; i++){
 		if (arr[i] == true){
